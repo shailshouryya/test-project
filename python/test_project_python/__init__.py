4c05be2aaea400993debc500ee42241896352405
slow-but-steady <slowbutsteady1234@gmail.com>
2023-02-20 14:00:21 -0800

    Add working `command_name` for `entry_points['console_scripts']`

--- python/test_project_python/__init__.py
+++ python/test_project_python/__init__.py
@@ -1,1 +1,8 @@
+def function_name():
+    print('Ran test_project_python.__init__.function_name')
+    return True
 
+
+if __name__ == '__main__':
+    print('__name__ == "__main__" is True for test_project_python.__init__')
+    function_name()
--- python/test_project_python/__main__.py
+++ python/test_project_python/__main__.py
@@ -1,1 +1,5 @@
 print('Entered test_project_python.__main__ :)')
+
+
+if __name__ == '__main__':
+    print('__name__ == "__main__" is True for test_project_python.__main__')
