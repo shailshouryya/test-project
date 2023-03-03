88e079da51d8f287a0cae3e75b0bcb849b11427c
slow-but-steady <slowbutsteady1234@gmail.com>
2023-03-02 20:48:10 -0800

    Add `make_subpackage` helper function for clarity

--- python/make_nested_py_modules.py
+++ python/make_nested_py_modules.py
@@ -21,29 +21,26 @@ def remove_directory(directory):
 def create_packages(package_letters, module_letters):
     for package_letter in package_letters:
         package_name = f'package_{package_letter}'
+        package_path = os.path.join('.', package_name)
         print(f'package_name is {package_name}')
-        os.makedirs(name=package_name, mode=511, exist_ok=False)
-        __init__filepath = os.path.join(package_name, '__init__.py')
-        with open(__init__filepath, mode='w', encoding='utf-8', buffering=-1) as file:
-            file.write('\n')
-        for first_level_module in module_letters:
-            module_name = f'module_{first_level_module}'
-            module_file_path = os.path.join(package_name, f'{module_name}.py')
-            with open(module_file_path, mode='w', encoding='utf-8', buffering=-1) as file:
-                file.write(f'''print('{package_name}.{first_level_module}')\n''')
-                file.write('')
+        create_subpackage(package_path, package_name, module_letters, module_prefix='module_')
         for second_level in package_letters:
-            second_level_path = os.path.join(package_name, f'subpackage_{second_level}')
             second_level_name = f'{package_name}.subpackage_{second_level}'
-            os.makedirs(name=second_level_path, mode=511, exist_ok=False)
-            second_level__init__filepath = os.path.join(second_level_path, '__init__.py')
-            with open(second_level__init__filepath, mode='w', encoding='utf-8', buffering=-1) as file:
-                file.write('\n')
-            for second_level_module in module_letters:
-                second_level_module_name = f'{second_level_module}'
-                second_level_module_file_path = os.path.join(second_level_path, f'{second_level_module_name}.py')
-                with open(second_level_module_file_path, mode='w', encoding='utf-8', buffering=-1) as file:
-                    file.write(f'''print('{second_level_name}.{second_level_module}')\n''')
+            second_level_path = os.path.join(package_name, f'subpackage_{second_level}')
+            create_subpackage(second_level_path, second_level_name, module_letters, module_prefix='')
+
+
+def create_subpackage(package_path, package_name, module_letters, module_prefix):
+    os.makedirs(name=package_path, mode=511, exist_ok=False)
+    __init__filepath = os.path.join(package_path, '__init__.py')
+    with open(__init__filepath, mode='w', encoding='utf-8', buffering=-1) as file:
+        file.write('\n')
+    for module in module_letters:
+        module_name = f'{module_prefix}{module}'
+        module_file_path = os.path.join(package_path, f'{module_name}.py')
+        with open(module_file_path, mode='w', encoding='utf-8', buffering=-1) as file:
+            file.write(f'''print('{package_name}.{module}')\n''')
+            file.write('')
 
 
 if __name__ == '__main__':
