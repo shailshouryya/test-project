9168a88c8f29227ab460cef6a9693959b16fdeee
slow-but-steady <slowbutsteady1234@gmail.com>
2023-03-10 19:37:36 -0800

    Rename key `'module_prefix'` → `'module_prefixes'`
    - creates a Cartesian product of the
      `'module_prefixes'` and `'module_suffixes'`
      (the value for both keys should be a sequence of strings)

--- python/test_project_python/make_nested_py_modules.py
+++ python/test_project_python/make_nested_py_modules.py
@@ -11,25 +11,25 @@ def main(package_levels=None, start=1, end=2, directory_removal_prefixes=None):
             1: {
                 'subpackage_prefixes': ['package_'],
                 'subpackage_suffixes': ['a', 'b', 'c'],
-                'module_prefix': 'module_',
+                'module_prefixes': ['module_'],
                 'module_suffixes': ['a', 'b', 'c', 'd', 'e'],
             },
             2: {
                 'subpackage_prefixes': ['subpackage_'],
                 'subpackage_suffixes': ['a', 'b', 'c'],
-                'module_prefix': '',
+                'module_prefixes': [''],
                 'module_suffixes': ['a', 'b', 'c', 'd', 'e'],
             },
             3: {
                 'subpackage_prefixes': ['subpackage_'],
                 'subpackage_suffixes': ['a', 'b', 'c'],
-                'module_prefix': '',
+                'module_prefixes': [''],
                 'module_suffixes': ['a', 'b', 'c', 'd', 'e'],
             },
             4: {
                 'subpackage_prefixes': ['subpackage_'],
                 'subpackage_suffixes': ['a', 'b', 'c'],
-                'module_prefix': '',
+                'module_prefixes': [''],
                 'module_suffixes': ['a', 'b', 'c', 'd', 'e'],
             },
         }
@@ -57,14 +57,14 @@ def create_packages(package_levels, start, end, base_path, base_name, __init__fi
         current_subpackage_suffixes = current_package.get('subpackage_suffixes', ['_package'])
         for subpackage_prefix, subpackage_suffix in itertools.product(current_subpackage_prefixes, current_subpackage_suffixes):
             package_name, package_path      = determine_subpackage_info(subpackage_prefix, subpackage_suffix, base_path, base_name)
-            module_prefix , module_suffixes = determine_module_info_for_subpackage(current_package)
+            module_prefixes , module_suffixes = determine_module_info_for_subpackage(current_package)
             create_subpackage(package_path)
             for __init__file, __init__package_path in __init__files:
                 relative_subpackage_path, relative_subpackage_name = determine_relative_subpackage_info(package_name, __init__package_path)
                 __init__file.write(f'from {relative_subpackage_path} import {relative_subpackage_name}\n')
             subpackage__init__filepath = os.path.join(package_path, '__init__.py')
             with open(subpackage__init__filepath, mode='w', encoding='utf-8', buffering=-1) as subpackage__init__file:
-                create_modules_for_subpackage(package_path, package_name, module_suffixes, module_prefix, subpackage__init__file)
+                create_modules_for_subpackage(package_path, package_name, module_suffixes, module_prefixes, subpackage__init__file)
                 __init__files.append((subpackage__init__file, package_name))
                 create_packages(package_levels, start+1, end, base_path=package_path, base_name=package_name, __init__files=__init__files)
                 __init__files.pop()
@@ -80,8 +80,8 @@ def determine_subpackage_info(subpackage_prefix, subpackage_suffix, base_path, b
 
 def determine_module_info_for_subpackage(current_package):
     module_suffixes = current_package.get('module_suffixes', ['_name'])
-    module_prefix   = current_package.get('module_prefix', 'module_')
-    return module_prefix , module_suffixes
+    module_prefixes = current_package.get('module_prefixes', ['module_'])
+    return module_prefixes, module_suffixes
 
 
 def determine_relative_subpackage_info(package_name, __init__package_path):
@@ -117,8 +117,8 @@ def create_subpackage(package_path, mode=511, exist_ok=False):
     os.makedirs(name=package_path, mode=mode, exist_ok=exist_ok)
 
 
-def create_modules_for_subpackage(package_path, package_name, module_suffixes, module_prefix, subpackage__init__file):
-    for module_suffix in module_suffixes:
+def create_modules_for_subpackage(package_path, package_name, module_suffixes, module_prefixes, subpackage__init__file):
+    for module_prefix, module_suffix in itertools.product(module_prefixes, module_suffixes):
         module_name = f'{module_prefix}{module_suffix}'
         subpackage__init__file.write(f'from . import {module_name}\n')
         module_file_path = os.path.join(package_path, f'{module_name}.py')
