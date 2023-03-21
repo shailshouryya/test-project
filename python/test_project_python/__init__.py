83f350412eae81ab3e17e6cb36c670d070104cfa
slow-but-steady <slowbutsteady1234@gmail.com>
2023-03-20 22:33:28 -0700

    Add type hinting `return` value for `function_name` function

--- python/test_project_python/__init__.py
+++ python/test_project_python/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.0.2.post5'
 
 print('Entered test_project_python.__init__ :)')
 
-def function_name():
+def function_name() -> bool:
     print('Ran test_project_python.__init__.function_name')
     return True
 
