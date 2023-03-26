0f1cf2096dad29b790a35939eb62039dd507d5cb
slow-but-steady <slowbutsteady1234@gmail.com>
2023-03-25 22:49:53 -0700

    Rename function `validate_instance_is_from_class` → `verify_instance_is_from_type`
    - increases clarity

--- python/test_project_python/package_builder.py
+++ python/test_project_python/package_builder.py
@@ -18,11 +18,11 @@ def main(
     has_at_least_one_of:           Collection[str]               = ['__init__.py'],
 ) -> None:
     none_type = type(None)
-    validate_instance_is_from_class(package_levels, (none_type, Mapping), 'package_levels')
-    validate_instance_is_from_class(start, int, 'start')
-    validate_instance_is_from_class(end,   int, 'end')
-    validate_instance_is_from_class(directory_removal_prefixes, (none_type, Collection), 'directory_removal_prefixes')
-    validate_instance_is_from_class(has_at_least_one_of,        (none_type, Collection), 'has_at_least_one_of')
+    verify_instance_is_from_type(package_levels, (none_type, Mapping), 'package_levels')
+    verify_instance_is_from_type(start, int, 'start')
+    verify_instance_is_from_type(end,   int, 'end')
+    verify_instance_is_from_type(directory_removal_prefixes, (none_type, Collection), 'directory_removal_prefixes')
+    verify_instance_is_from_type(has_at_least_one_of,        (none_type, Collection), 'has_at_least_one_of')
     if directory_removal_prefixes is None: directory_removal_prefixes = ['package_']
     remove_script_created_contents(directory_removal_prefixes, has_at_least_one_of)
     if package_levels is None:
@@ -55,7 +55,7 @@ def main(
     create_packages(package_levels, start, end, base_path='.', base_name='', __init__files=[])
 
 
-def validate_instance_is_from_class(
+def verify_instance_is_from_type(
     obj:                 Any,
     acceptable_types:    Union[T, Tuple],
     variable_name:       str,
@@ -115,13 +115,13 @@ def create_packages(
         current_package             = package_levels.get(current_level, {})
         current_subpackage_prefixes = current_package.get('subpackage_prefixes', [f'level_{current_level}_'])
         current_subpackage_suffixes = current_package.get('subpackage_suffixes', ['_package'])
-        validate_instance_is_from_class(current_subpackage_prefixes, Collection, f'''package_levels[{current_level}]['subpackage_prefixes']''')
-        validate_instance_is_from_class(current_subpackage_suffixes, Collection, f'''package_levels[{current_level}]['subpackage_suffixes']''')
+        verify_instance_is_from_type(current_subpackage_prefixes, Collection, f'''package_levels[{current_level}]['subpackage_prefixes']''')
+        verify_instance_is_from_type(current_subpackage_suffixes, Collection, f'''package_levels[{current_level}]['subpackage_suffixes']''')
         for subpackage_prefix, subpackage_suffix in itertools.product(current_subpackage_prefixes, current_subpackage_suffixes):
             package_name, package_path        = determine_subpackage_info(subpackage_prefix, subpackage_suffix, base_path, base_name)
             module_prefixes , module_suffixes = determine_module_info_for_subpackage(current_package)
-            validate_instance_is_from_class(module_prefixes, Collection, f'''package_levels[{current_level}]['module_prefixes']''')
-            validate_instance_is_from_class(module_suffixes, Collection, f'''package_levels[{current_level}]['module_suffixes']''')
+            verify_instance_is_from_type(module_prefixes, Collection, f'''package_levels[{current_level}]['module_prefixes']''')
+            verify_instance_is_from_type(module_suffixes, Collection, f'''package_levels[{current_level}]['module_suffixes']''')
             create_subpackage(package_path)
             for __init__file, __init__package_path in __init__files:
                 relative_subpackage_path, relative_subpackage_name = determine_relative_subpackage_info(package_name, __init__package_path)
