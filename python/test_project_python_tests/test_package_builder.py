5584ebf996c34d0f3645ee577c5bbc61d4acc496
slow-but-steady <slowbutsteady1234@gmail.com>
2023-03-26 13:22:49 -0700

    Add `data_verify_instance_is_from_single_acceptable_type` variable
    - uses tuple unpacking on the result of the generator expression
      to dynamically create and format the test cases

--- python/test_project_python_tests/test_package_builder.py
+++ python/test_project_python_tests/test_package_builder.py
@@ -42,18 +42,25 @@ def format_result(function, input_arguments, description):
     return f'{description}: {function.__name__}{input_arguments}'
 
 none_type  = type(None)
+data_verify_instance_is_from_single_acceptable_type = [
+    ('s',   str,        'str_val'),
+    (1,     int,        'int_val'),
+    (1.0,   float,      'float_val'),
+    ([],    Collection, 'list_val'),
+    ((),    Collection, 'tuple_val'),
+    ({},    Collection, 'dict_val'),
+    (set(), Collection, 'set_val'),
+    ('s',   Collection, 'str_val'),
+    ({},    Mapping,    'dict_val'),
+]
+
 data_verify_instance_is_from_type = [
     verify_instance_is_from_type,
     (
-        (('s',    str,                   'str_var'),      '`str`   input with string       as only acceptable type'),
-        ((1,      int,                    'int_var'),     '`int`   input with `int`        as only acceptable type'),
-        ((1.0,    float,                  'float_var'),   '`float` input with `float`      as only acceptable type'),
-        (([],     Collection,             'list_var'),    '`list`  input with `Collection` as only acceptable type'),
-        (((),     Collection,             'tuple_var'),   '`tuple` input with `Collection` as only acceptable type'),
-        (({},     Collection,             'dict_var'),    '`dict`  input with `Collection` as only acceptable type'),
-        ((set(),  Collection,             'set_var'),     '`set`   input with `Collection` as only acceptable type'),
-        (('s',    Collection,             'str_var'),     '`str`   input with `Collection` as only acceptable type'),
-        (({},     Mapping,                'dict_var'),    '`dict`  input with `Mapping`    as only acceptable type'),
+        *(
+            ((obj, acceptable_type, variable_name), f'{variable_name:<9} input with `{str(acceptable_type):<17}` as only acceptable type')
+            for obj, acceptable_type, variable_name in data_verify_instance_is_from_single_acceptable_type
+        ),
         (('s',   (str, none_type),       'str_var'),      '`str`   input with `str` and `None`        as acceptable types'),
         ((1,     (int, none_type),        'int_var'),     '`int`   input with `int` and `None`        as acceptable types'),
         ((1.0,   (float, none_type),      'float_var'),   '`float` input with `float` and `None`      as acceptable types'),
