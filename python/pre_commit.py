4a09f38421f0313c53fa1cc8f270d403f09b16ed
slow-but-steady <slowbutsteady1234@gmail.com>
2023-04-05 21:25:08 -0700

    Run `pylint` on changed `.py` files

--- python/pre_commit.py
+++ python/pre_commit.py
@@ -1,7 +1,13 @@
+import os
+import subprocess
+
 import mypy.main
+import pylint.lint
 
 
 def main():
+    changed_py_files = subprocess.getoutput('git diff --name-only --staged "*.py"').splitlines()
+    lint_changed_py_files(changed_py_files)
     package_configs = [
         ['-p', 'test_project_python'],
         ['-p', 'test_project_python_tests'],
@@ -9,6 +15,15 @@ def main():
     for package_config in package_configs:
         print(f'''mypy {' '.join(package_config)}''')
         mypy.main.main(args=package_config, clean_exit=True)
+
+
+def lint_changed_py_files(changed_py_files):
+    config_path    = os.path.join('python', '.pylintrc')
+    pylint_options = f'--rcfile={config_path}'
+    for changed_py_file in changed_py_files:
+        # pylint.run_pylint(pylint_options + [changed_py_file])
+        # # calls pylint.lint.Run(argv or sys.argv[1:])
+        pylint.lint.Run(args=[pylint_options, changed_py_file])
 
 
 
