33ad4719df5f953a6e8c1ecc8ce424f09f0af1a6
slow-but-steady <slowbutsteady1234@gmail.com>
2023-04-06 20:17:17 -0700

    Explicitly specify `acceptable_types` type hinting to expect a `Type` of `Any`
    - adresses the following `mypy` error:
    /path/to/test_project/python/test_project_python/package_builder.py: note: In function "verify_instance_is_from_type":
    /path/to/test_project/python/test_project_python/package_builder.py:61:32:61:32: error: Missing type parameters for generic type "Type"
    [type-arg]
            acceptable_types:    Union[Type, Tuple],
                                       ^

--- python/test_project_python/package_builder.py
+++ python/test_project_python/package_builder.py
@@ -57,7 +57,7 @@ def main(
 
 def verify_instance_is_from_type(
     obj:                 Any,
-    acceptable_types:    Union[Type, Tuple],
+    acceptable_types:    Union[Type[Any], Tuple],
     variable_name:       str,
 ) -> Optional[TypeError]:
     if isinstance(acceptable_types, tuple): formatted_acceptable_types = f'one of the following types: {acceptable_types}' # multiple acceptable types
